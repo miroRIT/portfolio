eaec9dbc14c9d8574ba22aa8b8cf40142c1cd4b0
Aayush gupta <ayush.98g@gmail.com>
2026-06-26 09:24:26 +0000

    Portfolio creation

---
+++ script.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector('.nav-toggle');
+const siteNav = document.querySelector('.site-nav');
+
+navToggle?.addEventListener('click', () => {
+  siteNav?.classList.toggle('open');
+});
